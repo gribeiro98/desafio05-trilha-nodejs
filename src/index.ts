af07f4971c5687b61e0514ace01e63f26bfe73f1
Gabriel Ribeiro <gabriel.ribeiro.bastos@hotmail.com>
2022-08-04 19:31:37 -0300

    Configurado swagger-ui-express

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
